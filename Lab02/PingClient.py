073642579f78f1aaa7bf1929e8daf4a2cec9d73d
YinYuWu <z5135190@student.unsw.edu.au>
2018-08-02 15:17:53 +1000

    finish pingclient

---
+++ Lab02/PingClient.py
@@ -0,0 +1,42 @@
+#python 2.7.12
+import sys
+import os
+import socket
+import time
+
+if (len(sys.argv) != 3):
+	print("Require Arguments: Host, Port")
+	os._exit(0)
+
+host = sys.argv[1]
+port = (int)(sys.argv[2])
+
+#set socket 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(1)
+
+#connect host
+#s.connect(host, port)
+
+#send 10 ping requests to server
+for sequence_number in range(0, 10):
+	#send requests
+	now_time = time.time()
+	ping_message = "PING" + " " + str(sequence_number) + " " + str(now_time) + "\r\n"
+	s.sendto(ping_message, (host, port))
+
+	#receive response
+	try:
+		data, address = s.recvfrom(1024)
+		recv_time = time.time()
+		diff = recv_time - now_time
+		rtt = (int)(1000 * diff + 0.5)
+		print("Ping to " + host + ", seq = " + str(sequence_number) + ", rtt = " + str(rtt) + "ms")
+		time.sleep(1)
+	except socket.timeout:
+		print("Ping to " + host + ", seq = " + str(sequence_number) + ", time out")
+
+
+s.close()
+
+
